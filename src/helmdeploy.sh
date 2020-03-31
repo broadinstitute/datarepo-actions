9ebf688f09bf3ce1963f4d86990b565f2e1f9f20
Mark Sarcevicz <smark@broadinstitute.org>
2020-03-31 12:43:43 -0400

    finally used helm_imagetag_update

--- src/helmdeploy.sh
+++ src/helmdeploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 helmdeploy () {
   eval $(cat env_vars)
-  if [[ "${google_zone}" != "" ]] && [[ "${k8_cluster}" != "" ]]; then
+  if [[ "${google_zone}" != "" ]] && [[ "${k8_cluster}" != "" ]] && [[ "${helm_imagetag_update}" != "" ]]; then
     helm plugin install https://github.com/thomastaylor312/helm-namespace
     helm repo add datarepo-helm https://broadinstitute.github.io/datarepo-helm
     helm repo update
@@ -11,7 +11,7 @@ helmdeploy () {
     helm namespace upgrade ${NAMESPACEINUSE}-jade datarepo-helm/datarepo --version=${helm_datarepo_chart_version} --install \
       --namespace ${NAMESPACEINUSE} -f \
       "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/${NAMESPACEINUSE}Deployment.yaml" \
-      --set "datarepo-${image}.image.tag=${GCR_TAG}"
+      --set "datarepo-${helm_imagetag_update}.image.tag=${GCR_TAG}"
   else
     echo "required var not defined for function helmdeploy"
     exit 1
