cb06d5d40641610163bef1658b15e469c123ec98
Mark Sarcevicz <smark@broadinstitute.org>
2020-03-31 11:38:12 -0400

    added sleeps to be safe

--- src/waitfordeployment.sh
+++ src/waitfordeployment.sh
@@ -2,6 +2,8 @@
 waitfordeployment () {
   eval $(cat env_vars)
   if [[ "${google_zone}" != "" ]] && [[ "${k8_cluster}" != "" ]]; then
+    # wait for deployment to happen
+    sleep 5
     if kubectl get deployments -n ${NAMESPACEINUSE} ${NAMESPACEINUSE}-jade-datarepo-ui -o jsonpath="{.status}" | grep unavailable; then
       sleep 10
       echo "pod unavailable waiting 10 seconds..."
@@ -9,6 +11,8 @@ waitfordeployment () {
     else
       pod=$(kubectl get deployments -n ${NAMESPACEINUSE} ${NAMESPACEINUSE}-jade-datarepo-ui -o jsonpath="{..metadata.name}")
       echo "${pod} has been deploy and in ready state"
+      #sleep 5 to be safe
+      sleep 5
     fi
   else
     echo "required var not defined for function waitfordeployment"
