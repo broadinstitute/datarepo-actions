d7d61f4015cd9b144db19a3c5e00132ff0f4e4d9
Jeremy Hert <jhert@broadinstitute.org>
2020-06-14 22:30:24 -0400

    try escaping the quotes

--- src/bumper.sh
+++ src/bumper.sh
@@ -113,7 +113,9 @@ bumper () {
 
     echo "line to change"
     SUFFIX=SNAPSHOT
-    VERSION_LINE=$(cat $INPUT_VERSION_FILE_PATH | grep -e "^${INPUT_VERSION_VARIABLE_NAME}")
+    VERSION_LINE=$(cat $INPUT_VERSION_FILE_PATH | \
+        grep -e "^${INPUT_VERSION_VARIABLE_NAME}" | \
+        sed "s/\'/\\\'/g")
     SED_COMMAND="s/${VERSION_LINE}/${INPUT_VERSION_VARIABLE_NAME} '${new}-${SUFFIX}'/"
     echo $SED_COMMAND
     sed -i .bak "$SED_COMMAND" $INPUT_VERSION_FILE_PATH
