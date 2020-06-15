4591e4d542f74f2f71534ac720e98e685039ba47
Jeremy Hert <jhert@broadinstitute.org>
2020-06-14 23:12:19 -0400

    ocjdskc

--- src/bumper.sh
+++ src/bumper.sh
@@ -113,14 +113,9 @@ bumper () {
 
     echo "line to change"
     SUFFIX=SNAPSHOT
-    VERSION_LINE=$(cat $INPUT_VERSION_FILE_PATH | \
-        grep -e "^${INPUT_VERSION_VARIABLE_NAME}" | \
-        sed "s/'/\\\'/g")
-    echo $VERSION_LINE
-    SED_COMMAND="s/${VERSION_LINE}/${INPUT_VERSION_VARIABLE_NAME} \'${new}-${SUFFIX}\'/"
-    echo $SED_COMMAND
-    set -x
-    sed -i "s/${VERSION_LINE}/${INPUT_VERSION_VARIABLE_NAME} \'${new}-${SUFFIX}\'/" $INPUT_VERSION_FILE_PATH
+    VERSION_LINE=$(cat $INPUT_VERSION_FILE_PATH | grep -e "^${INPUT_VERSION_VARIABLE_NAME}")
+    SED_COMMAND="s/${VERSION_LINE}/${INPUT_VERSION_VARIABLE_NAME} '${new}-${SUFFIX}'/"
+    sed -i $SED_COMMAND $INPUT_VERSION_FILE_PATH
     cat $INPUT_VERSION_FILE_PATH
     exit 0
 
