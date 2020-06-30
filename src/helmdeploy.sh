be7013dd6baedd1aef8eba06815ab3dbd94dfb80
Mark Sarcevicz <smark@broadinstitute.org>
2020-06-30 14:07:22 -0400

    added ifs for both api and ui and changed --set block to change the real path for image.tag

--- src/helmdeploy.sh
+++ src/helmdeploy.sh
@@ -15,19 +15,30 @@ helmdeploy () {
          --install --namespace ${NAMESPACEINUSE} -f \
          "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/gcloud-sqlproxy.yaml"
     sleep 3
-
-    helm namespace upgrade ${release_name}-datarepo-api datarepo-helm/datarepo-api --version=${helm_datarepo_api_chart_version} \
-         --install --namespace ${NAMESPACEINUSE} -f \
-         "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/datarepo-api.yaml" \
-         --set "datarepo-${helm_imagetag_update}.image.tag=${GCR_TAG}"
-    sleep 3
-
-    helm namespace upgrade ${release_name}-datarepo-ui datarepo-helm/datarepo-ui --version=${helm_datarepo_ui_chart_version} \
-         --install --namespace ${NAMESPACEINUSE} -f \
-         "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/datarepo-ui.yaml" \
-         --set "datarepo-${helm_imagetag_update}.image.tag=${GCR_TAG}"
-    sleep 3
-
+    if [[ "${helm_imagetag_update}" == "api" ]]; then
+      helm namespace upgrade ${release_name}-datarepo-api datarepo-helm/datarepo-api --version=${helm_datarepo_api_chart_version} \
+           --install --namespace ${NAMESPACEINUSE} -f \
+           "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/datarepo-api.yaml" \
+           --set "image.tag=${GCR_TAG}"
+      sleep 3
+    else
+      helm namespace upgrade ${release_name}-datarepo-api datarepo-helm/datarepo-api --version=${helm_datarepo_api_chart_version} \
+           --install --namespace ${NAMESPACEINUSE} -f \
+           "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/datarepo-api.yaml"
+      sleep 3
+    fi
+    if [[ "${helm_imagetag_update}" == "ui" ]]; then
+      helm namespace upgrade ${release_name}-datarepo-ui datarepo-helm/datarepo-ui --version=${helm_datarepo_ui_chart_version} \
+           --install --namespace ${NAMESPACEINUSE} -f \
+           "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/datarepo-ui.yaml" \
+           --set "image.tag=${GCR_TAG}"
+      sleep 3
+    else
+      helm namespace upgrade ${release_name}-datarepo-ui datarepo-helm/datarepo-ui --version=${helm_datarepo_ui_chart_version} \
+           --install --namespace ${NAMESPACEINUSE} -f \
+           "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/datarepo-ui.yaml"
+      sleep 3
+    fi
     helm namespace upgrade ${release_name}-oidc-proxy datarepo-helm/oidc-proxy --version=${helm_oidc_proxy_chart_version} \
          --install --namespace ${NAMESPACEINUSE} -f \
          "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/oidc-proxy.yaml"
