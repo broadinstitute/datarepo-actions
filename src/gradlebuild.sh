1fefeea4dc8bde7d005a0bdcc3d8d69a433ef8e4
Mark Sarcevicz <smark@broadinstitute.org>
2020-07-15 15:47:47 -0400

    stop cluster auth if no k8 specificed
    stop cluster auth if no k8 specificed
    
    syntax
    
    fixed thens
    
    changed if for tagger
    
    added export for test
    
    sourced function gradlebuild instead of just executing it
    
    testing source
    
    testing source
    
    testing with parseinputs
    
    testing arg passin
    
    readded source and passin arg
    
    tired need more coffee
    
    added export so env var
    
    if statement was going to empty var
    
    run git command from source dir and not in ui folder
    
    optd for -C for git
    
    added alpharelease arg to be passed in

--- src/gradlebuild.sh
+++ src/gradlebuild.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 gradlebuild () {
-  if [[ "${gcr_google_project}" != "" ]]; then
+  if [[ -n "$1" ]]; then
+    export DEV_PROJECT=$1
+  fi
+  if [[ -n "$2" ]]; then
+    export alpharelease=$2
+  fi
+  if [[ "${DEV_PROJECT}" != "" ]]; then
     export GOOGLE_APPLICATION_CREDENTIALS=jade-dev-account.json
     if [[ -n "${alpharelease}" ]]; then
-      GCR_TAG="${alpharelease}-alpha"
+      printf "running gradle with alpha tag ${alpharelease}"
+      export GCR_TAG="${alpharelease}-alpha"
     else
-      GCR_TAG=$(git rev-parse --short HEAD)
+      export GCR_TAG=$(git rev-parse --short HEAD)
+      printf "Using tag from local dir $GCR_TAG"
     fi
     echo "export GCR_TAG=${GCR_TAG}"  >> env_vars
     ./gradlew jib
