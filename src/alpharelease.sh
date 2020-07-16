ea3fb9afe9b223a38deae774b7658cde63922757
Mark Sarcevicz <smark@broadinstitute.org>
2020-07-16 10:34:00 -0400

    sourced function gradlebuild instead of just executing it

--- src/alpharelease.sh
+++ src/alpharelease.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 alpharelease () {
-  export DEV_PROJECT="${INPUT_GCR_GOOGLE_PROJECT}"
   if [[ -n "${alpharelease}" ]] && [[ -n "${DEV_PROJECT}" ]]; then
     printf "Running alpharelease for tag ${alpharelease}\n"
     cd jade-data-repo-ui
     uitag=$(git rev-parse --short HEAD)
     # echo "::set-output name=ui-tag::$ui-tag"
     gcloud container images add-tag gcr.io/broad-jade-dev/jade-data-repo-ui:${uitag} \
-      gcr.io/broad-jade-dev/jade-data-repo-ui:"${alpharelease}-alpha"
+      gcr.io/broad-jade-dev/jade-data-repo-ui:"${alpharelease}-alpha" --quiet
+    source gradlebuild.sh
     gradlebuild
   else
     printf "alpha release not sucessful missing vars"
