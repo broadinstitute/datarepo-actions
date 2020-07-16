678c6e7d111ff19e2c66d1b10a1da5cb7fad7887
Mark Sarcevicz <smark@broadinstitute.org>
2020-07-16 11:02:43 -0400

    readded source and passin arg

--- src/alpharelease.sh
+++ src/alpharelease.sh
@@ -8,6 +8,8 @@ alpharelease () {
     # echo "::set-output name=ui-tag::$ui-tag"
     gcloud container images add-tag gcr.io/broad-jade-dev/jade-data-repo-ui:${uitag} \
       gcr.io/broad-jade-dev/jade-data-repo-ui:"${alpharelease}-alpha" --quiet
+# source and run gradlebuild
+    source ${scriptDir}/gradlebuild.sh
     gradlebuild "${DEV_PROJECT}"
   else
     printf "alpha release not sucessful missing vars"
