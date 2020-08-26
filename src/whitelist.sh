4f1722f7c11d4eba086c94a9b26acb6a3a6f6e21
Shelby Holden <sholden@broadinstitute.org>
2020-08-26 07:59:20 -0400

    Revert "re-work env variable"
    This reverts commit b1d1b91bedd89c8cd72d515462488e347cbfd2a9.

--- src/whitelist.sh
+++ src/whitelist.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 whitelist () {
-  if [[ $k8_cluster != "" ]]; then
-    CUR_IPS=$(gcloud container clusters describe $k8_cluster --format json | \
+  if [[ "${env.k8_cluster}" != "" ]]; then
+    CUR_IPS=$(gcloud container clusters describe ${env.k8_cluster} --format json | \
       jq -r '[.masterAuthorizedNetworksConfig.cidrBlocks[] | .cidrBlock]')
       RUNNER_IP=$(curl 'https://api.ipify.org/?format=text' | xargs printf '[ "%s/32" ]')
       NEW_IPS=$(printf '%s\n' $CUR_IPS $RUNNER_IP | jq -s -r 'add | unique | join(",")')
-    gcloud container clusters update $k8_cluster \
+    gcloud container clusters update ${env.k8_cluster} \
       --enable-master-authorized-networks \
       --master-authorized-networks ${NEW_IPS}
   else
