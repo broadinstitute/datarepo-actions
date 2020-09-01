f6766c7165141423b0d5115af6e2134f5483d8f1
Shelby Holden <sholden@broadinstitute.org>
2020-09-01 15:51:27 -0400

    switch to PR smoke test suite

--- src/gradleinttest.sh
+++ src/gradleinttest.sh
@@ -66,7 +66,7 @@ gradleinttest () {
       #cat ${HOME}/${google_application_credentials}
       #printf ${HOME}/${google_application_credentials}
       cd ${GITHUB_WORKSPACE}/${workingDir}/datarepo-clienttests      
-      ./gradlew runTest --args="suites/BasicSmoke.json tmp/TestRunnerResults"
+      ./gradlew runTest --args="suites/PRSmokeTests.json tmp/TestRunnerResults"
       cd ${GITHUB_WORKSPACE}/${workingDir}
     else
       ./gradlew assemble
