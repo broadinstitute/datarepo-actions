fa58f44597446b78e0f09034b5b4cd4de223418b
Shelby Holden <sholden@broadinstitute.org>
2020-09-02 16:55:39 -0400

    clean up commands as code review sugguested

--- src/gradletestrunnersmoketest.sh
+++ src/gradletestrunnersmoketest.sh
@@ -6,13 +6,11 @@ gradletestrunnersmoketest () {
     export GOOGLE_APPLICATION_CREDENTIALS=${google_application_credentials}
     export GOOGLE_CLOUD_PROJECT=${google_project}
     export GOOGLE_CLOUD_DATA_PROJECT=${google_data_project}
-
-    cd ${GITHUB_WORKSPACE}/${workingDir}/datarepo-client
-    ../gradlew clean assemble
-    cd ${GITHUB_WORKSPACE}/${workingDir}
-    export ORG_GRADLE_PROJECT_datarepoclientjar=.$(ls ./datarepo-client/build/libs/*jar)
     export TEST_RUNNER_SERVER_SPECIFICATION_FILE="${NAMESPACEINUSE}.json"
-    cd ${GITHUB_WORKSPACE}/${workingDir}/datarepo-clienttests      
+    cd ${GITHUB_WORKSPACE}/${workingDir}/datarepo-client
+    ../gradlew clean assemble  
+    cd ${GITHUB_WORKSPACE}/${workingDir}/datarepo-clienttests
+    export ORG_GRADLE_PROJECT_datarepoclientjar=$(ls ../datarepo-client/build/libs/*jar)
     ./gradlew runTest --args="suites/PRSmokeTests.json tmp/TestRunnerResults"
     cd ${GITHUB_WORKSPACE}/${workingDir}
   else
