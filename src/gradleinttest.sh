ee34b8cb7a3d861c68d04b913863635400f88dfc
Shelby Holden <sholden@broadinstitute.org>
2020-09-01 20:28:24 -0400

    Revert "Confirm error is happening b/c pointing to jar"
    This reverts commit 406f01509ef727fbf353cdcf5e293d0b26010fe0.

--- src/gradleinttest.sh
+++ src/gradleinttest.sh
@@ -56,10 +56,10 @@ gradleinttest () {
     # required for tests
     if [[ "${test_to_run}" == "testPerf" ]]; then
       printf "perf test\n"
-      #cd ${GITHUB_WORKSPACE}/${workingDir}/datarepo-client
-      #../gradlew clean assemble
-      #cd ${GITHUB_WORKSPACE}/${workingDir}
-      #export ORG_GRADLE_PROJECT_datarepoclientjar=.$(ls ./datarepo-client/build/libs/*jar)
+      cd ${GITHUB_WORKSPACE}/${workingDir}/datarepo-client
+      ../gradlew clean assemble
+      cd ${GITHUB_WORKSPACE}/${workingDir}
+      export ORG_GRADLE_PROJECT_datarepoclientjar=.$(ls ./datarepo-client/build/libs/*jar)
       export TEST_RUNNER_SERVER_SPECIFICATION_FILE="${NAMESPACEINUSE}.json"
       # export GOOGLE_APPLICATION_CREDENTIALS=${GITHUB_WORKSPACE}/${workingDir}/jade-dev-account.json
       printf "list credentials"
