9dbf875773dd1c1f929185ddcb43756f93397c5b
Shelby Holden <sholden@broadinstitute.org>
2020-09-03 11:02:42 -0400

    remove check for variables

--- src/gradletestrunnersmoketest.sh
+++ src/gradletestrunnersmoketest.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 gradletestrunnersmoketest () {
-
-  if [[ -n "${google_project}" ]] && [ -f ${GOOGLE_APPLICATION_CREDENTIALS} ]; then
-    export TEST_RUNNER_SERVER_SPECIFICATION_FILE="${NAMESPACEINUSE}.json"
-    cd ${GITHUB_WORKSPACE}/${workingDir}/datarepo-client
-    ../gradlew clean assemble  
-    cd ${GITHUB_WORKSPACE}/${workingDir}/datarepo-clienttests
-    export ORG_GRADLE_PROJECT_datarepoclientjar=$(ls ../datarepo-client/build/libs/*jar)
-    ./gradlew runTest --args="suites/PRSmokeTests.json tmp/TestRunnerResults"
-    cd ${GITHUB_WORKSPACE}/${workingDir}
-  else
-    echo "missing vars for function gradletestrunnersmoketest"
-    exit 1
-  fi
+  export TEST_RUNNER_SERVER_SPECIFICATION_FILE="${NAMESPACEINUSE}.json"
+  cd ${GITHUB_WORKSPACE}/${workingDir}/datarepo-client
+  ../gradlew clean assemble  
+  cd ${GITHUB_WORKSPACE}/${workingDir}/datarepo-clienttests
+  export ORG_GRADLE_PROJECT_datarepoclientjar=$(ls ../datarepo-client/build/libs/*jar)
+  ./gradlew runTest --args="suites/PRSmokeTests.json tmp/TestRunnerResults"
+  cd ${GITHUB_WORKSPACE}/${workingDir}
 }
