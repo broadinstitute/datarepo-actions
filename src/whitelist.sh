daee1625b2de1a4a448799a73244b5e753f70ebe
Shelby Holden <sholden@broadinstitute.org>
2020-09-03 13:49:15 -0400

    clean up bash script

--- src/whitelist.sh
+++ src/whitelist.sh
@@ -6,9 +6,18 @@ whitelist () {
       jq -r '[.masterAuthorizedNetworksConfig.cidrBlocks[] | .cidrBlock]')
       RUNNER_IP=$(curl 'https://api.ipify.org/?format=text' | xargs printf '[ "%s/32" ]')
       NEW_IPS=$(printf '%s\n' $CUR_IPS $RUNNER_IP | jq -s -r 'add | unique | join(",")')
-    for i in {1..5}; do gcloud container clusters update ${k8_cluster} \
+    for i in {1..5}
+    do
+      if gcloud container clusters update ${K8_CLUSTER} \
         --enable-master-authorized-networks \
-        --master-authorized-networks ${NEW_IPS} && echo "successful whitelist" && break || echo "retrying whitelist" && sleep 15; done
+        --master-authorized-networks ${NEW_IPS}; then
+          echo "successful whitelist"
+          break
+      else
+        echo "failed to whitelist"
+        sleep 15
+      fi
+    done
   else
     echo "Required var not defined for function whitelist"
     exit 1
