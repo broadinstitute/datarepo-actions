e3ccd2a4f04ed66dbeef4673a7d00670e2961f16
Shelby Holden <sholden@broadinstitute.org>
2020-09-03 15:01:23 -0400

    update echo messages

--- src/whitelist.sh
+++ src/whitelist.sh
@@ -8,7 +8,7 @@ whitelist () {
       NEW_IPS=$(printf '%s\n' $CUR_IPS $RUNNER_IP | jq -s -r 'add | unique | join(",")')
     for i in {1..5}; do gcloud container clusters update ${k8_cluster} \
         --enable-master-authorized-networks \
-        --master-authorized-networks ${NEW_IPS} && break || sleep 15 && echo "retrying"; done
+        --master-authorized-networks ${NEW_IPS} && echo "successful whitelist" && break || echo "retrying whitelist" && sleep 15; done
   else
     echo "Required var not defined for function whitelist"
     exit 1
