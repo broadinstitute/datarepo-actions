4d28617d2cb8b5ccdaa648ac428e7583cfac11cd
Shelby Holden <sholden@broadinstitute.org>
2020-09-08 13:56:33 -0400

    Update src/whitelist.sh
    Co-authored-by: fboulnois <fboulnois@users.noreply.github.com>

--- src/whitelist.sh
+++ src/whitelist.sh
@@ -6,8 +6,7 @@ whitelist () {
       jq -r '[.masterAuthorizedNetworksConfig.cidrBlocks[] | .cidrBlock]')
       RUNNER_IP=$(curl 'https://api.ipify.org/?format=text' | xargs printf '[ "%s/32" ]')
       NEW_IPS=$(printf '%s\n' $CUR_IPS $RUNNER_IP | jq -s -r 'add | unique | join(",")')
-    for i in {1..5}
-    do
+    for i in {1..5}; do
       if gcloud container clusters update ${k8_cluster} \
         --enable-master-authorized-networks \
         --master-authorized-networks ${NEW_IPS}; then
