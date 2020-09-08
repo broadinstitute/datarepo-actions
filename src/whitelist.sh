d2509da975e1b6a08493de6653883fcce8b7f842
Shelby Holden <sholden@broadinstitute.org>
2020-09-08 11:05:33 -0400

    Add whitelist retry back

--- src/whitelist.sh
+++ src/whitelist.sh
@@ -6,9 +6,21 @@ whitelist () {
       jq -r '[.masterAuthorizedNetworksConfig.cidrBlocks[] | .cidrBlock]')
       RUNNER_IP=$(curl 'https://api.ipify.org/?format=text' | xargs printf '[ "%s/32" ]')
       NEW_IPS=$(printf '%s\n' $CUR_IPS $RUNNER_IP | jq -s -r 'add | unique | join(",")')
-    gcloud container clusters update ${k8_cluster} \
-      --enable-master-authorized-networks \
-      --master-authorized-networks ${NEW_IPS}
+    for i in {1..5}
+    do
+      if gcloud container clusters update ${K8_CLUSTER} \
+        --enable-master-authorized-networks \
+        --master-authorized-networks ${NEW_IPS}; then
+          echo "successful whitelist"
+          break
+      else
+        echo "failed to whitelist"
+        sleep 15
+        if [ i == 5 ]; then
+          echo "FAILURE TO WHITELIST"
+          exit 1
+      fi
+    done
   else
     echo "Required var not defined for function whitelist"
     exit 1
