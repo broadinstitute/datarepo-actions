fe68cbbd04e561d3a0b9efea486af28cd23c440b
Mariko Medlock <mariko.medlock@gmail.com>
2020-09-22 14:52:39 -0400

    Use PRSmokeTests measurement list.

--- src/gradletestrunnersmoketest.sh
+++ src/gradletestrunnersmoketest.sh
@@ -6,16 +6,16 @@ gradletestrunnersmoketest () {
   echo "Building Data Repo client library"
   ../gradlew clean assemble  
   cd ${GITHUB_WORKSPACE}/${workingDir}/datarepo-clienttests
-  export ORG_GRADLE_PROJECT_datarepoclientjar=$(ls ../datarepo-client/build/libs/*jar)
-  echo "ORG_GRADLE_PROJECT_datarepoclientjar = ${ORG_GRADLE_PROJECT_datarepoclientjar}"
 
   echo "Running spotless and spotbugs"
   ./gradlew spotlessCheck
   ./gradlew spotbugsMain
 
   echo "Setting Test Runner environment variables"
+  export ORG_GRADLE_PROJECT_datarepoclientjar=$(ls ../datarepo-client/build/libs/*jar)
   export TEST_RUNNER_SERVER_SPECIFICATION_FILE="${NAMESPACEINUSE}.json"
   export TEST_RUNNER_KEY_DIRECTORY_PATH="${GITHUB_WORKSPACE}"
+  echo "ORG_GRADLE_PROJECT_datarepoclientjar = ${ORG_GRADLE_PROJECT_datarepoclientjar}"
   echo "TEST_RUNNER_SERVER_SPECIFICATION_FILE = ${TEST_RUNNER_SERVER_SPECIFICATION_FILE}"
   echo "TEST_RUNNER_KEY_DIRECTORY_PATH = ${TEST_RUNNER_KEY_DIRECTORY_PATH}"
 
@@ -23,7 +23,7 @@ gradletestrunnersmoketest () {
   ./gradlew runTest --args="suites/PRSmokeTests.json tmp/TestRunnerResults"
 
   echo "Collecting measurements"
-  ./gradlew collectMeasurements --args="AllMeasurements.json tmp/TestRunnerResults"
+  ./gradlew collectMeasurements --args="PRSmokeTests.json tmp/TestRunnerResults"
 
   echo "Uploading results"
   ./gradlew uploadResults --args="BroadJadeDev.json tmp/TestRunnerResults"
