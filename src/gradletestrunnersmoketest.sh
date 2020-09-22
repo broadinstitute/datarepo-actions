b19a2fb4c3617b00606a7362a3a445e608922853
Mariko Medlock <mariko.medlock@gmail.com>
2020-09-22 14:56:15 -0400

    Change env var name to include SA.

--- src/gradletestrunnersmoketest.sh
+++ src/gradletestrunnersmoketest.sh
@@ -14,10 +14,10 @@ gradletestrunnersmoketest () {
   echo "Setting Test Runner environment variables"
   export ORG_GRADLE_PROJECT_datarepoclientjar=$(ls ../datarepo-client/build/libs/*jar)
   export TEST_RUNNER_SERVER_SPECIFICATION_FILE="${NAMESPACEINUSE}.json"
-  export TEST_RUNNER_KEY_DIRECTORY_PATH="${GITHUB_WORKSPACE}"
+  export TEST_RUNNER_SA_KEY_DIRECTORY_PATH="${GITHUB_WORKSPACE}"
   echo "ORG_GRADLE_PROJECT_datarepoclientjar = ${ORG_GRADLE_PROJECT_datarepoclientjar}"
   echo "TEST_RUNNER_SERVER_SPECIFICATION_FILE = ${TEST_RUNNER_SERVER_SPECIFICATION_FILE}"
-  echo "TEST_RUNNER_KEY_DIRECTORY_PATH = ${TEST_RUNNER_KEY_DIRECTORY_PATH}"
+  echo "TEST_RUNNER_SA_KEY_DIRECTORY_PATH = ${TEST_RUNNER_SA_KEY_DIRECTORY_PATH}"
 
   echo "Running test suite"
   ./gradlew runTest --args="suites/PRSmokeTests.json tmp/TestRunnerResults"
