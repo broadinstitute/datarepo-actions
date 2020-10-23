1d4845218bc48aca1a6d52deaffa47e5b0dc6b2e
Mark Sarcevicz <smark@broadinstitute.org>
2020-10-23 13:45:49 -0400

    TEMP CHANGE TO RAW BRANCH PATH FOR TESTING WILL REVERT

--- src/helmdeploy.sh
+++ src/helmdeploy.sh
@@ -4,7 +4,7 @@ helmdeploy () {
   if [[ "${google_zone}" != "" ]] && [[ "${K8_CLUSTER}" != "" ]] && [[ "${helm_imagetag_update}" != "" ]]; then
     helm namespace upgrade ${NAMESPACEINUSE}-create-secret-manager-secret datarepo-helm/create-secret-manager-secret --version=${helm_secret_chart_version} \
       --install --namespace ${NAMESPACEINUSE} -f \
-      "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/create-secret-manager-secret.yaml"
+      "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/ms-integrationrework/integration/${NAMESPACEINUSE}/create-secret-manager-secret.yaml"
 
     # Delete the previous API deployment
     helm delete --namespace ${NAMESPACEINUSE} ${NAMESPACEINUSE}-jade-datarepo-api
@@ -13,35 +13,35 @@ helmdeploy () {
 
     helm namespace upgrade ${release_name}-gcloud-sqlproxy datarepo-helm/gcloud-sqlproxy --version=${helm_gcloud_sqlproxy_chart_version} \
          --install --namespace ${NAMESPACEINUSE} -f \
-         "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/gcloud-sqlproxy.yaml"
+         "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/ms-integrationrework/integration/${NAMESPACEINUSE}/gcloud-sqlproxy.yaml"
     sleep 3
     if [[ "${helm_imagetag_update}" == "api" ]]; then
       helm namespace upgrade ${release_name}-datarepo-api datarepo-helm/datarepo-api --version=${helm_datarepo_api_chart_version} \
            --install --namespace ${NAMESPACEINUSE} -f \
-           "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/datarepo-api.yaml" \
+           "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/ms-integrationrework/integration/${NAMESPACEINUSE}/datarepo-api.yaml" \
            --set "image.tag=${GCR_TAG}"
       sleep 3
     else
       helm namespace upgrade ${release_name}-datarepo-api datarepo-helm/datarepo-api --version=${helm_datarepo_api_chart_version} \
            --install --namespace ${NAMESPACEINUSE} -f \
-           "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/datarepo-api.yaml"
+           "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/ms-integrationrework/integration/${NAMESPACEINUSE}/datarepo-api.yaml"
       sleep 3
     fi
     if [[ "${helm_imagetag_update}" == "ui" ]]; then
       helm namespace upgrade ${release_name}-datarepo-ui datarepo-helm/datarepo-ui --version=${helm_datarepo_ui_chart_version} \
            --install --namespace ${NAMESPACEINUSE} -f \
-           "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/datarepo-ui.yaml" \
+           "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/ms-integrationrework/integration/${NAMESPACEINUSE}/datarepo-ui.yaml" \
            --set "image.tag=${GCR_TAG}"
       sleep 3
     else
       helm namespace upgrade ${release_name}-datarepo-ui datarepo-helm/datarepo-ui --version=${helm_datarepo_ui_chart_version} \
            --install --namespace ${NAMESPACEINUSE} -f \
-           "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/datarepo-ui.yaml"
+           "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/ms-integrationrework/integration/${NAMESPACEINUSE}/datarepo-ui.yaml"
       sleep 3
     fi
     helm namespace upgrade ${release_name}-oidc-proxy datarepo-helm/oidc-proxy --version=${helm_oidc_proxy_chart_version} \
          --install --namespace ${NAMESPACEINUSE} -f \
-         "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/oidc-proxy.yaml"
+         "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/ms-integrationrework/integration/${NAMESPACEINUSE}/oidc-proxy.yaml"
     sleep 3
 
   else
