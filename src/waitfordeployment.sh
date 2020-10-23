b9af5c5b93db65c2b45195e7da41fca2fd850560
Mark Sarcevicz <smark@broadinstitute.org>
2020-10-23 13:18:28 -0400

    changed for new deployment

--- src/waitfordeployment.sh
+++ src/waitfordeployment.sh
@@ -4,12 +4,12 @@ waitfordeployment () {
   if [[ "${google_zone}" != "" ]] && [[ "${K8_CLUSTER}" != "" ]]; then
     # wait for deployment to happen
     sleep 5
-    if kubectl get deployments -n ${NAMESPACEINUSE} ${NAMESPACEINUSE}-jade-datarepo-ui -o jsonpath="{.status}" | grep unavailable; then
+    if kubectl get deployments -n ${NAMESPACEINUSE} ${NAMESPACEINUSE}-datarepo-ui -o jsonpath="{.status}" | grep unavailable; then
       sleep 10
       echo "pod unavailable waiting 10 seconds..."
       waitfordeployment
     else
-      pod=$(kubectl get deployments -n ${NAMESPACEINUSE} ${NAMESPACEINUSE}-jade-datarepo-ui -o jsonpath="{..metadata.name}")
+      pod=$(kubectl get deployments -n ${NAMESPACEINUSE} ${NAMESPACEINUSE}-datarepo-ui -o jsonpath="{..metadata.name}")
       echo "${pod} has been deploy and in ready state"
       #sleep 5 to be safe
       sleep 5
