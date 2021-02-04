d7d9b54056fccb869cfabe16fef7cf237bc572b8
Shelby Holden <sholden@broadinstitute.org>
2021-02-04 09:41:07 -0500

    DO NOT MERGE

--- src/bumper.sh
+++ src/bumper.sh
@@ -6,7 +6,7 @@ bumper () {
     # config
     default_semvar_bump=${DEFAULT_BUMP:-minor}
     with_v=${WITH_V:-false}
-    release_branches=${INPUT_RELEASE_BRANCHES:-develop,master}
+    release_branches=${INPUT_RELEASE_BRANCHES:-develop,master,sh-dr-1593-part-2-fix}
     custom_tag=${CUSTOM_TAG}
     source=${SOURCE:-.}
     dryrun=${DRY_RUN:-false}
