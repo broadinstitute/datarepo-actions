2266e6186526017281a9240fee31a137f0e80cc8
Shelby Holden <sholden@broadinstitute.org>
2021-02-24 15:02:15 -0500

    shellcheck changes

--- actions/merger/entrypoint.sh
+++ actions/merger/entrypoint.sh
@@ -11,15 +11,15 @@ COMMIT_MESSAGE="${COMMIT_MESSAGE:-"commit merged via github action"}"
 MERGE_BRANCH=${MERGE_BRANCH:-develop}
 SWITCH_DIRECTORIES=${SWITCH_DIRECTORIES:-"false"}
 
-if ${switch_directories} ; then
+if ${SWITCH_DIRECTORIES} ; then
   echo "[INFO] change directory to ${GITHUB_WORKSPACE}/${GITHUB_REPO}"
-  cd ${GITHUB_WORKSPACE}/${GITHUB_REPO}
+  cd "${GITHUB_WORKSPACE}"/"${GITHUB_REPO}"
 fi
 
 echo "[INFO] git config email to ${GITHUB_USER_EMAIL}"
-git config --global user.email ${GITHUB_USER_EMAIL}
+git config --global user.email "${GITHUB_USER_EMAIL}"
 echo "[INFO] git config name ${GITHUB_USER_NAME}"
-git config --global user.name ${GITHUB_USER_NAME}
+git config --global user.name "${GITHUB_USER_NAME}"
 echo "[INFO] git add all"
 git add .
 echo "[INFO] commit w/ msg"
@@ -27,7 +27,7 @@ git commit -m "${COMMIT_MESSAGE}"
 echo "[INFO] Show ref"
 git show-ref
 echo "[INFO] git push ${MERGE_BRANCH}"
-git push origin ${MERGE_BRANCH}
+git push origin "${MERGE_BRANCH}"
 echo "[INFO] post push"
 commit=$(git rev-parse HEAD)
 echo "[INFO] commit: $commit"
