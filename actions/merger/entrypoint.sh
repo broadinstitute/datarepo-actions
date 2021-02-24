3afee9910abb4a4c74b93ef91e2c14483996cd5a
Shelby Holden <sholden@broadinstitute.org>
2021-02-24 16:32:37 -0500

    remove unnecessary quotes

--- actions/merger/entrypoint.sh
+++ actions/merger/entrypoint.sh
@@ -7,9 +7,9 @@ set -x
 GITHUB_REPO=${GITHUB_REPO:-jade-data-repo}
 GITHUB_USER_EMAIL=${GITHUB_USER_EMAIL:-broadbot@broadinstitute.org}
 GITHUB_USER_NAME=${GITHUB_USER_NAME:-broadbot}
-COMMIT_MESSAGE="${COMMIT_MESSAGE:-"commit merged via github action"}"
+COMMIT_MESSAGE=${COMMIT_MESSAGE:-commit merged via github action}
 MERGE_BRANCH=${MERGE_BRANCH:-develop}
-SWITCH_DIRECTORIES=${SWITCH_DIRECTORIES:-"false"}
+SWITCH_DIRECTORIES=${SWITCH_DIRECTORIES:-false}
 
 if ${SWITCH_DIRECTORIES} ; then
   cd "${GITHUB_WORKSPACE}"/"${GITHUB_REPO}"
