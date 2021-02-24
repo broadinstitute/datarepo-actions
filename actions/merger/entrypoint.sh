b490d92118465532cdb59253c58753c22067586a
Shelby Holden <sholden@broadinstitute.org>
2021-02-23 23:57:57 -0500

    define variables

--- actions/merger/entrypoint.sh
+++ actions/merger/entrypoint.sh
@@ -2,19 +2,27 @@
 
 set -e
 
-echo "HELLO FROM merger action"
+# config
+github_repo=${GITHUB_REPO:-jade-data-repo}
+github_user_email=${GITHUB_USER_EMAIL:-broadbot@broadinstitute.org}
+github_user_name=${GITHUB_USER_NAME:-broadbot}
+github_workspace=${GITHUB_WORKSPACE}
+commit_msg=${COMMIT_MESSAGE:-"commit merged via github action"}
+merge_branch=${MERGE_BRANCH:-develop}
 
-echo "[INFO] git config email to broadbot@broadinstitute.org"
-git config --global user.email broadbot@broadinstitute.org
-echo "[INFO] git config name broadbot"
-git config --global user.name broadbot
+echo "[INFO] change directory to ${github_workspace}/${github_repo}"
+cd ${github_workspace}/${github_repo}
+
+echo "[INFO] git config email to ${github_user_email}"
+git config --global user.email ${github_user_email}
+echo "[INFO] git config name ${github_user_name}"
+git config --global user.name ${github_user_name}
 echo "[INFO] git add all"
-cd ${GITHUB_WORKSPACE}/jade-data-repo
-git add .github/workflows/alpha-promotion.yaml
+git add .
 echo "[INFO] commit w/ msg"
-git commit -m "Datarepo chart version update"
+git commit -m ${commit_msg}
 echo "[INFO] git push"
-git push origin develop
+git push origin ${merge_branch}
 echo "[INFO] post push"
 commit=$(git rev-parse HEAD)
 echo "[INFO] commit: $commit"
