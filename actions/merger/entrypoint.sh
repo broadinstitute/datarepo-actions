f00aef5ed93c7131619e89d850a6e859f6470256
Shelby Holden <sholden@broadinstitute.org>
2021-02-23 23:22:33 -0500

    merge

--- actions/merger/entrypoint.sh
+++ actions/merger/entrypoint.sh
@@ -3,3 +3,18 @@
 set -e
 
 echo "HELLO FROM merger action"
+
+echo "[INFO] git config email to broadbot@broadinstitute.org"
+git config --global user.email broadbot@broadinstitute.org
+echo "[INFO] git config name broadbot"
+git config --global user.name broadbot
+echo "[INFO] git add all"
+cd ${GITHUB_WORKSPACE}/jade-data-repo
+git add .github/workflows/alpha-promotion.yaml
+echo "[INFO] commit w/ msg"
+git commit -m "Datarepo chart version update"
+echo "[INFO] git push"
+git push origin develop
+echo "[INFO] post push"
+commit=$(git rev-parse HEAD)
+echo "[INFO] commit: $commit"
