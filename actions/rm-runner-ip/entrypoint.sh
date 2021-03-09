7dad9d8b255e5092a518c17c0bf6851ea3c2898a
Florian Boulnois <fboulnoi@broadinstitute.org>
2021-03-09 15:33:03 -0500

    Try cluster update in retry loop

--- actions/rm-runner-ip/entrypoint.sh
+++ actions/rm-runner-ip/entrypoint.sh
@@ -10,8 +10,15 @@ RESTORE_IPS=$(echo "${CUR_IPS}" | \
     jq -r --arg ip "${RUNNER_IP}" '[ .[] | select(startswith($ip) | not) ] | unique | join(",")')
 
 # restore the original list of authorized IPs if they exist
-gcloud container clusters update "${K8_CLUSTER}" \
-    --enable-master-authorized-networks \
-    --master-authorized-networks "${RESTORE_IPS}"
+while true; do
+    if gcloud container clusters update "${K8_CLUSTER}" \
+        --enable-master-authorized-networks \
+        --master-authorized-networks "${RESTORE_IPS}"; then
+        break
+    else
+        echo "Failed to remove runner from allowlist -- Retrying"
+        sleep 15
+    fi
+done
 
 echo "Successfully removed ${RUNNER_IP} from allowlist"
