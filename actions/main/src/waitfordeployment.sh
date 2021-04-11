09e4832bd7469a6df22bae06707d772013b0da2e
Shelby Holden <sholden@broadinstitute.org>
2021-04-11 07:23:36 -0400

    increase wait

--- actions/main/src/waitfordeployment.sh
+++ actions/main/src/waitfordeployment.sh
@@ -3,14 +3,15 @@ waitfordeployment () {
   eval $(cat env_vars)
   if [[ "${google_zone}" != "" ]] && [[ "${K8_CLUSTER}" != "" ]]; then
     # wait for deployment to happen
-    sleep 5
+    echo "sleep 15 seconds to wait for ui pod to go down after helm deploy..."
+    sleep 15
     if kubectl get deployments -n ${NAMESPACEINUSE} ${NAMESPACEINUSE}-jade-datarepo-ui -o jsonpath="{.status}" | grep unavailable; then
       sleep 10
-      echo "pod unavailable waiting 10 seconds..."
+      echo "ui pod unavailable waiting 10 seconds..."
       waitfordeployment
     else
       pod=$(kubectl get deployments -n ${NAMESPACEINUSE} ${NAMESPACEINUSE}-jade-datarepo-ui -o jsonpath="{..metadata.name}")
-      echo "${pod} has been deploy and in ready state"
+      echo "${pod} has been deployed and in ready state"
       #sleep 5 to be safe
       sleep 5
     fi
