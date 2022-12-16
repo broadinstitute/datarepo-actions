9e12bcc1f9f9ca2f2b654350d6ff5580a05f7197
Chelsea Hoover <60902147+choover-broad@users.noreply.github.com>
2022-12-16 10:47:09 -0500

    Update main.sh

--- actions/main/src/main.sh
+++ actions/main/src/main.sh
@@ -132,13 +132,13 @@ googleAuth () {
     gcloud auth configure-docker --quiet
     echo 'Set google sdk to SA user'
     if [[ -n "${K8_CLUSTER}" ]]; then
-      echo "gcloud version:"
+      echo "> gcloud version:"
       gcloud version
-      echo "kubectl version:"
-      kubectl version
-      echo "gke-gcloud-auth-plugin version:"
+      echo "> kubectl version:"
+      kubectl version --client=true
+      echo "> gke-gcloud-auth-plugin version:"
       gke-gcloud-auth-plugin --version
-      echo "Authenticating kubectl to ${K8_CLUSTER}..."
+      echo "> Authenticating kubectl to ${K8_CLUSTER}..."
       gcloud --verbosity debug container clusters get-credentials ${K8_CLUSTER} --zone ${google_zone}
     fi
   else
